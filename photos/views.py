ff410790587aef27001cfb1c2763aa89df0faf1b
msayracode <msayracode@gmail.com>
2026-03-30 21:01:15 +0300

    Render data in template

--- photos/views.py
+++ photos/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def gallery(request):
-    return render(request, 'photos/gallery.html')
+    categories = Category.objects.all()
+    photos = Photo.objects.all()
+
+    context = {'categories':categories, 'photos':photos}
+
+    return render(request, 'photos/gallery.html',context)
 
 
 
 
 def viewPhoto(request, pk):
-    return render(request, 'photos/photo.html')
+    photo = Photo.objects.get(id=pk)
+
+    context = {'photo':photo}
+    return render(request, 'photos/photo.html',context)
 
 
 
